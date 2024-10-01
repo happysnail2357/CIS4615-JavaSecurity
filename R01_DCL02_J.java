fbceb36b913cdec2ce52aff4c1b68f287aa24444
Paul Puhnaty <pa764915@ucf.edu>
2024-10-01 13:05:12 -0400

    Fixed selected vulnerabilities with compliant code

--- R01_DCL02_J.java
+++ R01_DCL02_J.java
@@ -2,12 +2,13 @@ List<Integer> list = Arrays.asList(new Integer[] {13, 14, 15});
 boolean first = true;
  
 System.out.println("Processing list...");
-for (Integer i: list) {
+for (final Integer i: list) {
+  Integer item = i;
   if (first) {
     first = false;
-    i = new Integer(99);
+    item = new Integer(99);
   }
-  System.out.println(" New item: " + i);
+  System.out.println(" New item: " + item);
   // Process i
 }
  
--- R09_LCK07_J.java
+++ R09_LCK07_J.java
@@ -1,5 +1,6 @@
 final class BankAccount {
   private double balanceAmount;  // Total amount in bank account
+  private static final Object lock = new Object();
  
   BankAccount(double balance) {
     this.balanceAmount = balance;
@@ -8,16 +9,13 @@ final class BankAccount {
   // Deposits the amount from this object instance
   // to BankAccount instance argument ba
   private void depositAmount(BankAccount ba, double amount) {
-    synchronized (this) {
-      synchronized (ba) {
-        if (amount > balanceAmount) {
-          throw new IllegalArgumentException(
-               "Transfer cannot be completed"
-          );
-        }
-        ba.balanceAmount += amount;
-        this.balanceAmount -= amount;
+    synchronized (lock) {
+      if (amount > balanceAmount) {
+        throw new IllegalArgumentException(
+            "Transfer cannot be completed");
       }
+      ba.balanceAmount += amount;
+      this.balanceAmount -= amount;
     }
   }
  
@@ -25,7 +23,7 @@ final class BankAccount {
     final BankAccount second, final double amount) {
  
     Thread transfer = new Thread(new Runnable() {
-        public void run() {
+        @Override public void run() {
           first.depositAmount(second, amount);
         }
     });
